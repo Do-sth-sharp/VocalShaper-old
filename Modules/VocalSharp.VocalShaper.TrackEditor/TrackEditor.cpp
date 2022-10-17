2b64792254084e2a3dbf5f81ab2d634314e16c80
無常 <3142324836@qq.com>
2022-10-17 15:46:55 +0800

    Update TrackEditor

--- Modules/VocalSharp.VocalShaper.TrackEditor/TrackEditor.cpp
+++ Modules/VocalSharp.VocalShaper.TrackEditor/TrackEditor.cpp
@@ -1,12 +1,79 @@
-#include "TrackEditor.h"
+﻿#include "TrackEditor.h"
+#include <libJModule.h>
 
 TrackEditor::TrackEditor()
 	:EditorBase()
+{
+	//获取屏幕属性接口
+	this->screenSizeFunc =
+		jmadf::GetInterface<juce::Component*, juce::Rectangle<int>&>(
+			"WuChang.JMADF.Device", "GetScreenSize"
+			);
+
+	//以下获取界面属性
+	bool result = false;
+	//color
+	jmadf::CallInterface<const juce::String&, const juce::String&, const juce::String&, juce::Colour&, bool&>(
+		"WuChang.JMADF.LookAndFeelConfigs", "GetColor",
+		"main", "color", "background", this->colors.background, result
+		);
+
+	//size
+	jmadf::CallInterface<const juce::String&, const juce::String&, const juce::String&, double&, bool&>(
+		"WuChang.JMADF.LookAndFeelConfigs", "GetNumber",
+		"main", "size", "width-trackHead", this->sizes.width_trackHead, result
+		);
+	jmadf::CallInterface<const juce::String&, const juce::String&, const juce::String&, double&, bool&>(
+		"WuChang.JMADF.LookAndFeelConfigs", "GetNumber",
+		"main", "size", "height-timeRuler", this->sizes.height_timeRuler, result
+		);
+
+	//position
+	//scale
+	
+	//resource
+}
+
+void TrackEditor::resized()
 {
 
 }
 
 void TrackEditor::paint(juce::Graphics& g)
 {
-	//g.fillAll(juce::Colours::khaki);
+	g.fillAll(this->colors.background);
+}
+
+void TrackEditor::projectChanged(const vocalshaper::ProjectProxy* ptr)
+{
+	juce::ScopedWriteLock locker(this->projectLock);
+	this->project = const_cast<vocalshaper::ProjectProxy*>(ptr);
+	//TODO 刷新
+}
+
+void TrackEditor::setEditMode(bool editMode)
+{
+	this->editModeFlag = editMode;
+}
+
+void TrackEditor::setToolID(uint8_t toolID)
+{
+	this->toolID = toolID;
+}
+
+void TrackEditor::trackChanged(int trackID)
+{
+	juce::ScopedWriteLock locker(this->projectLock);
+	this->trackID = trackID;
+	//TODO 刷新
+}
+
+void TrackEditor::setTotalLength(vocalshaper::ProjectTime totalLength)
+{
+	//TODO
+}
+
+void TrackEditor::setCurrentPosition(vocalshaper::ProjectTime currentTime)
+{
+	//TODO
 }
--- Modules/VocalSharp.VocalShaper.TrackEditor/TrackEditor.h
+++ Modules/VocalSharp.VocalShaper.TrackEditor/TrackEditor.h
@@ -8,11 +8,40 @@ public:
 	TrackEditor();
 	~TrackEditor() override = default;
 
+	void resized() override;
 	void paint(juce::Graphics& g) override;
 
 public:
-
+	void projectChanged(const vocalshaper::ProjectProxy* ptr) override;
+	void setEditMode(bool editMode) override;
+	void setToolID(uint8_t toolID) override;
+	void trackChanged(int trackID) override;
+	void setTotalLength(vocalshaper::ProjectTime totalLength) override;
+	void setCurrentPosition(vocalshaper::ProjectTime currentTime) override;
 
 private:
+	struct Colors final
+	{
+		juce::Colour background;
+	}colors;//界面颜色
+	struct Size final
+	{
+		double width_trackHead;
+		double height_timeRuler;
+	}sizes;//控件大小
+	struct Scales final
+	{
+	}scales;//控件缩放
+
+	std::function<void(juce::Component*, juce::Rectangle<int>&)> screenSizeFunc;
+	juce::Rectangle<int> screenSize;
+
+	vocalshaper::ProjectProxy* project = nullptr;
+	int trackID = -1;
+	juce::ReadWriteLock projectLock;
+
+	bool editModeFlag = false;
+	uint8_t toolID = 1;
+
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(TrackEditor)
 };
