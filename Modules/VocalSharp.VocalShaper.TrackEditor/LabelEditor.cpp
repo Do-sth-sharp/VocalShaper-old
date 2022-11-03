11484db0f6dfe4ce548b9ff27c4483b7b3370a5e
無常 <3142324836@qq.com>
2022-11-03 19:51:22 +0800

    Add LabelEditor

---
+++ Modules/VocalSharp.VocalShaper.TrackEditor/LabelEditor.cpp
@@ -0,0 +1,56 @@
+﻿#include "LabelEditor.h"
+#include <libJModule.h>
+
+LabelEditor::LabelEditor()
+	: Component("Label Editor")
+{
+	//获取屏幕属性接口
+	this->screenSizeFunc =
+		jmadf::GetInterface<juce::Component*, juce::Rectangle<int>&>(
+			"WuChang.JMADF.Device", "GetScreenSize"
+			);
+
+	//获取翻译器
+	jmadf::CallInterface<std::function<const juce::String(const juce::String&)>&>(
+		"WuChang.JMADF.Translates", "GetFunc",
+		this->tr
+		);
+
+	//初始化标签类型下拉框
+	this->comboBox = std::make_unique<juce::ComboBox>("Label Type");
+	this->comboBox->addItemList({ "LUA", "INI", "XML", "JSON" }, 1);
+	this->comboBox->setSelectedItemIndex(1);
+	this->comboBox->onChange = [this] {this->checkLabelData(); };
+	this->addAndMakeVisible(this->comboBox.get());
+
+	//TODO 初始化编辑器与数据模型
+}
+
+void LabelEditor::setProject(vocalshaper::ProjectProxy* project)
+{
+	juce::ScopedWriteLock locker(this->projectLock);
+	this->project = project;
+	this->refreshLabel();
+}
+
+void LabelEditor::setLabelIndex(int index)
+{
+	juce::ScopedWriteLock locker(this->projectLock);
+	this->index = index;
+	this->refreshLabel();
+}
+
+void LabelEditor::resized()
+{
+
+}
+
+void LabelEditor::refreshLabel()
+{
+	//TODO 加载标签数据并检查
+}
+
+void LabelEditor::checkLabelData()
+{
+	//TODO 检查标签数据
+}
---
+++ Modules/VocalSharp.VocalShaper.TrackEditor/LabelEditor.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <JuceHeader.h>
+#include <libVocalShaper.h>
+
+class LabelEditor final : public juce::Component
+{
+public:
+	LabelEditor();
+	~LabelEditor() override = default;
+
+	void setProject(vocalshaper::ProjectProxy* project);
+	void setLabelIndex(int index);
+
+	void resized() override;
+
+private:
+	std::function<void(juce::Component*, juce::Rectangle<int>&)> screenSizeFunc;
+	std::function<const juce::String(const juce::String&)> tr;
+
+	std::unique_ptr<juce::ComboBox> comboBox = nullptr;
+	std::unique_ptr<juce::CodeEditorComponent> labelEditor = nullptr;
+	std::unique_ptr<juce::CodeDocument> document = nullptr;
+	std::unique_ptr<juce::Label> result = nullptr;
+	std::unique_ptr<juce::TextButton> okButton = nullptr;
+
+	vocalshaper::ProjectProxy* project = nullptr;
+	int index = -1;
+	juce::ReadWriteLock projectLock;
+
+	void refreshLabel();
+	void checkLabelData();
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(LabelEditor)
+};
